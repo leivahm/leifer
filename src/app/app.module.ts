c50c27ab778fc6cc59869619826a425ef91455c2
leivahm <leivahmarcelo@gmail.com>
2022-10-27 18:27:35 -0300

    Leifer's Angular course

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { CardComponent } from './card/card.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    CardComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
